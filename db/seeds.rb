1fd9514124a54dc0148871ae9654cb0a6ee018eb
Alisia Michel <cc0-11@outlook.de>
2019-07-03 19:23:07 +0200

    searchbar not working yet, everything else fine

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting old Flats..."
+Flat.destroy_all
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Rooftopflat close to skyline of NewYork',
+  address: '21A East Valley NewYork 1234',
+  description: 'Little flet with amazing big rooftop balkony. Have a barbeque and look over the skyline!',
+  price_per_night: 99,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Biological Treehouse with garden in Berlin',
+  address: 'Neuköln Prinzessinengarten 10',
+  description: 'Sleep and live inbetween trees but with the city right next to you. Perfect for everyone who looks for the best of both.',
+  price_per_night: 60,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Loft in the sky between the clouds of beautiful Madird',
+  address: '3 Sky Castle Madrid W7 13T',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 99,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Farmhouse Gaukönigshofen village center',
+  address: 'Zehnthofstraße 11, 97253 Gaukönigshofen',
+  description: 'This nice farm is embedded in a little village in bavaria Gaukönigshofen. Enjoy your time wihtout any disturbances.',
+  price_per_night: 40,
+  number_of_guests: 6
+)
+puts "finished!"
